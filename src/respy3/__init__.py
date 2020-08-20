b6986f8579ff22bfa911e612ab5fa7aff543f1c2
Harrison Morgan <harrison.morgan@gmail.com>
2020-08-20 18:29:00 -0500

    Use package metadata for __version__

--- src/respy3/__init__.py
+++ src/respy3/__init__.py
@@ -1,1 +1,8 @@
-__version__ = '0.1.0'
+"""Respy3 is a protocol reader/writer for RESP3."""
+
+from importlib import metadata
+
+try:
+    __version__ = metadata.version(__name__)
+except Exception:  # pragma: no cover
+    __version__ = "unknown"
