28c897077429a7a51e64138c245f58eb137718b5
Harrison88 <harrison.morgan@gmail.com>
2020-08-20 21:23:30 -0500

    Update test_respy3.py
    Use metadata to check the correct __version__ is set

--- tests/test_respy3.py
+++ tests/test_respy3.py
@@ -1,5 +1,10 @@
+"""Test the version number is set."""
+
+from importlib import metadata
+
 from respy3 import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    """It has the correct __version__."""
+    assert __version__ == metadata.version("respy3")
